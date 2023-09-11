7b1f695284efde75995762d4f14233eff8754900
Nivaldo DeMello <melzdemello@gmail.com>
2023-09-11 14:00:08 -0400

    Commit from desktop

--- app.py
+++ app.py
@@ -4,40 +4,40 @@ app = Flask(__name__)
 
 JOBS = [
     {
-        'id': 1,
-        'title': 'Data Analyst',
-        'location': 'Bengaluru, India',
-        'salary': '$50,000',
+        "id": 1,
+        "title": "Data Analyst",
+        "location": "Bengaluru, India",
+        "salary": "$50,000",
     },
     {
-        'id': 2,
-        'title': 'Data Scientist',
-        'location': 'Dehli, India',
-        'salary': '$75,000',
+        "id": 2,
+        "title": "Data Scientist",
+        "location": "Dehli, India",
+        "salary": "$75,000",
     },
     {
-        'id': 3,
-        'title': 'Frontend Engineer',
-        'location': 'Remote',
+        "id": 3,
+        "title": "Frontend Engineer",
+        "location": "Remote",
     },
     {
-        'id': 4,
-        'title': 'Backend Engineer',
-        'location': 'San Francisco, USA',
-        'salary': '$65,000',
+        "id": 4,
+        "title": "Backend Engineer",
+        "location": "San Francisco, USA",
+        "salary": "$65,000",
     },
 ]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html', jobs=JOBS)
+    return render_template("home.html", jobs=JOBS)
 
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
-  app.run('0.0.0.0', debug=True)
+    app.run("0.0.0.0", debug=True)
