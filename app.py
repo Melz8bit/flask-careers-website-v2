9b38353611fe892998cd000197b8ce36fad8f7f0
Melz8bit <37233103+Melz8bit@users.noreply.github.com>
2023-09-11 17:51:31 +0000

    Added Flask HTML template and Bootstrap

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
+
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Bengaluru, India',
+        'salary': '$50,000',
+    },
+    {
+        'id': 2,
+        'title': 'Data Scientist',
+        'location': 'Dehli, India',
+        'salary': '$75,000',
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Engineer',
+        'location': 'Remote',
+    },
+    {
+        'id': 4,
+        'title': 'Backend Engineer',
+        'location': 'San Francisco, USA',
+        'salary': '$65,000',
+    },
+]
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
