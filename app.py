0315f8e50cea1ca7d3eb6cc059cf71c98594cbc0
Melz8bit <37233103+Melz8bit@users.noreply.github.com>
2023-09-12 19:30:42 +0000

    Added job page, made webpage templates

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, render_template
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -13,6 +13,16 @@ def hello_world():
 @app.route("/api/jobs")
 def list_jobs():
   return jsonify(load_jobs_from_db())
+
+
+@app.route("/job/<id>")
+def get_job(id):
+  job = load_job_from_db(id)
+
+  if not job:
+    return "Not Found", 404
+
+  return render_template("jobpage.html", job=job)
 
 
 if __name__ == "__main__":
