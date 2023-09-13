930e327b816758e01d17ae6f11f126a155f1a521
Nivaldo DeMello <melzdemello@gmail.com>
2023-09-13 11:41:10 -0400

    Store application to database; added application files

--- app.py
+++ app.py
@@ -1,29 +1,41 @@
-from flask import Flask, jsonify, render_template
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, jsonify, render_template, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  jobs = load_jobs_from_db()
-  return render_template("home.html", jobs=jobs)
+    jobs = load_jobs_from_db()
+    return render_template("home.html", jobs=jobs)
 
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(load_jobs_from_db())
+    return jsonify(load_jobs_from_db())
 
 
 @app.route("/job/<id>")
 def get_job(id):
-  job = load_job_from_db(id)
+    job = load_job_from_db(id)
 
-  if not job:
-    return "Not Found", 404
+    if not job:
+        return "Not Found", 404
 
-  return render_template("jobpage.html", job=job)
+    return render_template("jobpage.html", job=job)
+
+
+@app.route("/job/<id>/apply", methods=["POST"])
+def apply_to_job(id):
+    data = request.form
+    job = load_job_from_db(id)
+    add_application_to_db(id, data)
+    return render_template(
+        "application_submitted.html",
+        application=data,
+        job=job,
+    )
 
 
 if __name__ == "__main__":
-  app.run("0.0.0.0", debug=True)
+    app.run("0.0.0.0", debug=True)
--- database.py
+++ database.py
@@ -1,39 +1,65 @@
 import sqlalchemy
 import os
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
 from sqlalchemy.util import deprecations
 
+load_dotenv()
+
 deprecations.SILENCE_UBER_WARNING = 1
 
-db_username = os.environ['DB_USERNAME']
-db_password = os.environ['DB_PASSWORD']
+db_username = os.environ["DB_USERNAME"]
+db_password = os.environ["DB_PASSWORD"]
 
 DB_CONNECTION_STRING = f"mysql+pymysql://{db_username}:{db_password}@aws.connect.psdb.cloud/flaskcareers?charset=utf8mb4"
 
-engine = create_engine(DB_CONNECTION_STRING,
-                       connect_args={"ssl": {
-                           "ssl_ca": "/etc/ssl/cert.pem",
-                       }})
+engine = create_engine(
+    DB_CONNECTION_STRING,
+    connect_args={
+        "ssl": {
+            "ssl_ca": "/etc/ssl/cert.pem",
+        }
+    },
+)
 
 
 def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
 
-    jobs = []
-    for row in result.all():
-      jobs.append(dict(row))
+        jobs = []
+        for row in result.all():
+            jobs.append(dict(row))
 
-    return jobs
+        return jobs
 
 
 def load_job_from_db(id):
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs where id = :val"), val=id)
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs where id = :val"), val=id)
 
-    row = result.all()
-    if len(row) == 0:
-      return None
+        row = result.all()
+        if len(row) == 0:
+            return None
 
-    return dict(row[0])
+        return dict(row[0])
+
+
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text(
+            """INSERT INTO applications 
+                (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) 
+                VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"""
+        )
+        conn.execute(
+            query,
+            job_id=job_id,
+            full_name=data["full_name"],
+            email=data["email"],
+            linkedin_url=data["linkedin_url"],
+            education=data["education"],
+            work_experience=data["work_experience"],
+            resume_url=data["resume_url"],
+        )
